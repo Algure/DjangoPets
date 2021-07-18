55abae29e71fb3b3890145cceb6c638ea394f330
Algure <ajirie98@gmail.com>
2021-07-18 17:40:06 +0100

    working html tag responses

--- adoptions/models.py
+++ adoptions/models.py
@@ -8,13 +8,16 @@ class Pet(models.Model):
     species = models.CharField(max_length=30)
     breed = models.CharField(max_length=30, blank=True)
     description = models.TextField()
-    sex = models.CharField(max_length=1, choices=SEX_CHOICES, blank=True)
+    # sex = models.TextField(max_length=1, choices=SEX_CHOICES, blank=True)
     submission_date = models.DateField()
     age = models.IntegerField(null = True)
     vaccinations = models.ManyToManyField('Vaccine', blank=True)
 
 class Vaccine(models.Model):
     name = models.CharField(max_length=50)
-    
+
+    def __str__(self):
+        return self.name
+
 
 
